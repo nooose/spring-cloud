2fbc253972afc820078f008c095fec30f80d50a6
mz01-seongjh <test@test.com>
2022-03-13 19:35:31 +0900

    포트 RequestMapping 값 수정

--- second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/controller/SecondServiceController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/second-service")
 public class SecondServiceController {
 
     @GetMapping("/welcome")
     public String welcome() {
-        return "Welcome to the First Service";
+        return "Welcome to the Second Service";
     }
 }
