63d03e225e82cae6991737954fc8c91535bcc240
mz01-seongjh <test@test.com>
2022-03-15 23:07:08 +0900

    update

--- first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstServiceController.java
@@ -1,10 +1,13 @@
 package com.example.firstservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 @RequestMapping("/first-service")
 public class FirstServiceController {
 
@@ -12,4 +15,16 @@ public class FirstServiceController {
     public String welcome() {
         return "Welcome to the First Service";
     }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Welcome to the First Service";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from First Service";
+    }
+
 }
